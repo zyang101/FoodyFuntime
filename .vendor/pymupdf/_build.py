69b874257c6a97a10d9d3af5be591b7bdbf27e4e
Renee Cai <rcai3@jh.edu>
2026-05-01 15:45:00 -0400

    Finished blog

---
+++ .vendor/pymupdf/_build.py
@@ -0,0 +1,8 @@
+mupdf_location = 'https://mupdf.com/downloads/archive/mupdf-1.26.10-source.tar.gz'
+pymupdf_version = '1.26.5'
+pymupdf_version_tuple = (1, 26, 5)
+pymupdf_git_sha = 'cc13f8081799c42674a15ef7af6ed90d43b95e89'
+pymupdf_git_diff = ''
+pymupdf_git_branch = 'main'
+swig_version = '4.3.1'
+swig_version_tuple = (4, 3, 1)
